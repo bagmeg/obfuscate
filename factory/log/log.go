9916af854fc9a82750539ba70cb0cb0a98d70aa4
bagmeg <joonsoo181005@gmail.com>
2022-09-10 16:56:54 +0900

    추상 팩토리 디자인 적용

---
+++ factory/log/log.go
@@ -0,0 +1,16 @@
+package log
+
+type LogObfuscator struct {
+}
+
+func NewLogObfuscator() *LogObfuscator {
+	return &LogObfuscator{}
+}
+
+func (l *LogObfuscator) Tokenize() {
+
+}
+
+func (l *LogObfuscator) Parse() {
+
+}
---
+++ factory/sql/sql.go
@@ -0,0 +1,16 @@
+package sql
+
+type SQLObfuscator struct {
+}
+
+func NewSQLObfuscator() *SQLObfuscator {
+	return &SQLObfuscator{}
+}
+
+func (s *SQLObfuscator) Tokenize() {
+
+}
+
+func (s *SQLObfuscator) Parse() {
+
+}
